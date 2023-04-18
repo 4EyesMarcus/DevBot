9c36c24bda07a2532797801734934a33f298035a
4EyesMarcus <106706446+4EyesMarcus@users.noreply.github.com>
2023-04-18 17:04:53 -0400

    Add files via upload

--- DevBot-main/modbot/moderation.py
+++ DevBot-main/modbot/moderation.py
@@ -3,6 +3,7 @@ import nextcord
 from nextcord import slash_command, Interaction
 from nextcord.ext import commands
 import os
+import json
 
 class moderation(commands.Cog):
     def __init__(self, bot):
@@ -11,43 +12,67 @@ class moderation(commands.Cog):
 #Moderation Commands
 
     @slash_command(name="ban", description="Ban a member of this discord", guild_ids=[1078827353444192406])
-    @commands.has_permissions(ban_members=True)
     async def ban(
         self, 
         ctx:Interaction, 
         member: nextcord.Member = nextcord.SlashOption(name='member', description='Please provide a reason'), 
         reason: str = nextcord.SlashOption(name='reason', description='Please provide a reason')
     ):
+        
+        if not ctx.user.guild_permissions.ban_members:
+            await ctx.send("You do not have the required permissions to use this command.")
+            return
+    
         if not reason: reason = "No reason"
         await member.ban(reason=reason)
         await ctx.response.send_message(f"{member} has been banned by {ctx.user.mention} for {reason}")
         
         
     @slash_command(name="unban", description="Unban a member of this discord", guild_ids=[1078827353444192406])
-    @commands.has_permissions(ban_members=True)
     async def unban(
         self, 
         ctx:Interaction, 
         member: nextcord.Member = nextcord.SlashOption(name='member', description='Please provide a reason'), 
     ):
+        if not ctx.user.guild_permissions.ban_members:
+            await ctx.send("You do not have the required permissions to use this command.")
         await ctx.guild.unban(user=member)
         await ctx.response.send_message(f"{member} has been unbanned")
-        
+
+
+    @slash_command(name="kick", description="Kick a member of this discord", guild_ids=[1078827353444192406])
+    async def kick(
+        self, 
+        ctx:Interaction, 
+        member: nextcord.Member = nextcord.SlashOption(name='member', description='Please provide a reason'), 
+):
+        if not ctx.user.guild_permissions.kick_members:
+            await ctx.response.send_message("You do not have the required permissions to use this command.")
+        else:
+            await ctx.guild.kick(user=member)
+            await ctx.response.send_message(f"{member} has been kicked")
+
+
 
     @slash_command(name="mute", description="Mute Member in text chat", guild_ids=[1078827353444192406])
-    @commands.has_permissions(mute_members=True)
     async def mute(self, ctx: Interaction, member: nextcord.Member, reason: str):
-        muted_role = nextcord.utils.get(ctx.guild.roles, name="muted")
-        await member.add_roles(muted_role)
-        await ctx.send(f"{member.mention} has been muted because {reason}")
+        if not ctx.user.guild_permissions.mute_members:
+            await ctx.send("You do not have the required permissions to use this command.")
+        else:
+            muted_role = nextcord.utils.get(ctx.guild.roles, name="muted")
+            await member.add_roles(muted_role)
+            await ctx.send(f"{member.mention} has been muted because {reason}")
         
         
         
     @slash_command(name="unmute", description="Unmute a member in discord",guild_ids=[1078827353444192406])
     async def unmute(self, ctx:Interaction, member:nextcord.Member):
-        muted_role = nextcord.utils.get(ctx.guild.roles, name="muted")
-        await member.remove_roles(muted_role)
-        await ctx.send(f"{member.mention} has been unmuted.")
+        if not ctx.user.guild_permissions.mute_members:
+            await ctx.send("You do not have the required permissions to use this command.")
+        else:
+            muted_role = nextcord.utils.get(ctx.guild.roles, name="muted")
+            await member.remove_roles(muted_role)
+            await ctx.send(f"{member.mention} has been unmuted.")
         
         
         
@@ -80,34 +105,108 @@ class moderation(commands.Cog):
         await interaction.response.send_message(f"Deleted {limit} messages.", ephemeral=True)
 
 
-#Banned Words Text File
-    async def check_message(self, message: nextcord.Message):
-        user_warnings = {} # dictionary to keep track of how many warnings each user has received
-        file_path = os.path.join(os.path.dirname(__file__), 'profanity.txt')
+    @slash_command(name="whitelist", description="Add or remove a word from the whitelist", guild_ids=[1078827353444192406])
+    async def whitelist(self, ctx: Interaction, action: str = None, word: str = None):
+        if not ctx.user.guild_permissions.administrator:
+            await ctx.send("You do not have the required permissions to use this command.")
+            return
+
+        profanity_path = os.path.join(os.path.dirname(__file__), 'profanity.txt')
+        whitelist_path = os.path.join(os.path.dirname(__file__), 'whitelist.txt')
+    
         try:
-            with open(file_path, 'r') as f:
+            with open(profanity_path, 'r') as f:
+                profanity = f.read().splitlines()
+        except FileNotFoundError:
+            await ctx.send("File not found: profanity.txt")
+            return
+        
+        try:
+            with open(whitelist_path, 'r') as f:
+                whitelist = f.read().splitlines()
+        except FileNotFoundError:
+            whitelist = []
+    
+        if action == "add":
+            if word in whitelist:
+                await ctx.send(f"{word} is already whitelisted.")
+                return
+            if word in profanity:
+                profanity.remove(word)
+                with open(profanity_path, 'w') as f:
+                    f.write('\n'.join(profanity))
+            whitelist.append(word)
+            with open(whitelist_path, 'w') as f:
+                f.write('\n'.join(whitelist))
+            await ctx.send(f"{word} has been added to the whitelist.")
+        
+        elif action == "remove":
+            if word not in whitelist:
+                await ctx.send(f"{word} is not currently whitelisted.")
+                return
+            whitelist.remove(word)
+            with open(whitelist_path, 'w') as f:
+                f.write('\n'.join(whitelist))
+            with open(profanity_path, 'a') as f:
+                f.write('\n' + word)
+            await ctx.send(f"{word} has been removed from the whitelist.")
+    
+        else:
+            await ctx.send("Invalid action. Please specify 'add' or 'remove' as the first argument.")
+
+#AutoMod
+    async def check_message(self, message: nextcord.Message):
+        profanity_file_path = os.path.join(os.path.dirname(__file__), 'profanity.txt')
+        whitelist_file_path = os.path.join(os.path.dirname(__file__), 'whitelist.txt')
+        warnings_file_path = os.path.join(os.path.dirname(__file__), 'user_warnings.json')
+
+        try:
+            with open(profanity_file_path, 'r') as f:
                 profanity = f.read().splitlines()
         except FileNotFoundError:
             print("File not found: profanity.txt")
             return
-    
+
+        try:
+            with open(whitelist_file_path, 'r') as f:
+                whitelist = f.read().splitlines()
+        except FileNotFoundError:
+            print("File not found: whitelist.txt")
+            whitelist = []
+
+        try:
+            with open(warnings_file_path, 'r') as f:
+                data = json.load(f)
+                user_warnings = data.get(str(message.author.id), 0)
+        except FileNotFoundError:
+            print("File not found: user_warnings.json")
+            user_warnings = 0
+
         for word in profanity:
-            if word in message.content.lower():
-                user_id = message.author.id
-                if user_id in user_warnings:
-                    user_warnings[user_id] += 1
-                    if user_warnings[user_id] == 2:
-                        muted_role = nextcord.utils.get(message.guild.roles, name='muted') # Replace 'Muted' with the name of your muted role
-                        if muted_role:
-                            await message.author.add_roles(muted_role, reason="Message contained a banned word.")
-                            await message.channel.send(f"{message.author.mention}, you have been muted for using a banned word.")
-                        else:
-                            await message.channel.send("Muted role not found.")
-                    elif user_warnings[user_id] >= 3:
-                        await message.author.ban(reason="Message contained a banned word. Next time you will be banned!")
-                        await message.channel.send(f"{message.author.mention}, you have been banned for using a banned word.")
+            if word in message.content.lower() and word not in whitelist:
+                if user_warnings >= 2:
+                    muted_role = nextcord.utils.get(message.guild.roles, name='muted')
+                    if muted_role:
+                        await message.author.add_roles(muted_role, reason="Message contained a banned word.")
+                        await message.channel.send(f"{message.author.mention}, you have been muted for using a banned word.")
+                    else:
+                        await message.channel.send("Muted role not found.")
+                elif user_warnings >= 1:
+                    await message.channel.send(f"{message.author.mention}, your message contains a banned word. This is your final warning before being muted.")
                 else:
-                    user_warnings[user_id] = 1
+                    await message.channel.send(f"{message.author.mention}, your message contains a banned word. Please refrain from using it.")
+
+                user_warnings += 1
+                data[str(message.author.id)] = user_warnings
+
+                with open(warnings_file_path, 'w') as f:
+                    json.dump(data, f)
+
+                break
+
+
+   
+
 
 
 
